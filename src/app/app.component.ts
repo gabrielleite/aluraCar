da253c642db5ed17c2716df6dad4955375745ac4
Gabriel Leite <gabrielcfcleite@gmail.com>
2017-12-20 21:44:32 -0200

    Solução do bug de acessar o menu sem ter feito login.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { HomePage } from '../pages/home/home';
 import { ListaAgendamentosPage } from '../pages/lista-agendamentos/lista-agendamentos';
 import { LoginPage } from '../pages/login/login';
 import { PerfilPage } from '../pages/perfil/perfil';
+import { UsuariosProvider } from '../providers/api/usuarios/usuarios.service';
 
 @Component({
   selector: 'myapp',
@@ -18,11 +19,14 @@ export class MyApp {
   rootPage:any = LoginPage;
 
   public paginas = [
-    {titulo: 'Agendamentos', icone: 'calendar', componente: ListaAgendamentosPage },
+    { titulo: 'Agendamentos', icone: 'calendar', componente: ListaAgendamentosPage },
     { titulo: 'Perfil', icone: 'person', componente: PerfilPage }
   ];
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    private _usuariosService: UsuariosProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -33,6 +37,10 @@ export class MyApp {
 
   abrePagina(pagina): void {
     this.nav.push(pagina);
+  }
+
+  get usuarioLogado() {
+    return this._usuariosService.obtemUsuarioLogado();
   }
 }
 
